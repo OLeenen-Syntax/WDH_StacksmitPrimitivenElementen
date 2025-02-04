3a1c7a196ff4b4499de76dc9829dcaa86c71b1ca
Okan Leenen <okan.leenen@syntax-institut.de>
2025-02-04 17:38:37 +0100

    Lebenslauf

---
+++ primitive_Elemente/primitive_Elemente/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  primitive_Elemente
+//
+//  Created by Okan Leenen on 04.02.25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
