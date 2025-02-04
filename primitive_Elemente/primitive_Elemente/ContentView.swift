fa45d73644a3a96619b56d18115819627f4f8bb8
Okan Leenen <okan.leenen@syntax-institut.de>
2025-02-04 17:08:26 +0100

    init

---
+++ primitive_Elemente/primitive_Elemente/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  primitive_Elemente
+//
+//  Created by Okan Leenen on 04.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ primitive_Elemente/primitive_Elemente/primitive_ElementeApp.swift
@@ -0,0 +1,17 @@
+//
+//  primitive_ElementeApp.swift
+//  primitive_Elemente
+//
+//  Created by Okan Leenen on 04.02.25.
+//
+
+import SwiftUI
+
+@main
+struct primitive_ElementeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
